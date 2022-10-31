716ef36be5a50a543c2a02d6ee1042ec39ac0d21
Rayannecristinar <rayanne.cristinar@gmail.com>
2022-10-31 18:13:58 -0300

    desafio cloud parking

---
+++ src/main/java/dio/CloudParking/controller/ParkingController.java
@@ -0,0 +1,77 @@
+package dio.CloudParking.controller;
+
+import dio.CloudParking.controller.dto.ParkingCreateDTO;
+import dio.CloudParking.controller.dto.ParkingDTO;
+import dio.CloudParking.controller.mapper.ParkingMapper;
+import dio.CloudParking.model.Parking;
+import dio.CloudParking.service.ParkingService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking")
+@Api(tags = "ParkingController" )
+public class ParkingController {
+
+    private final ParkingService parkingService;
+    private final ParkingMapper parkingMapper;
+
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
+        this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
+    }
+
+
+    @GetMapping
+    @ApiOperation("Find all parkings")
+    public ResponseEntity<List<ParkingDTO>> findAll(){
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation("Find all By Id")
+    public ResponseEntity<ParkingDTO> findById(@PathVariable String id){
+        Parking parking = parkingService.findById(id);
+        ParkingDTO result = parkingMapper.toParkingDTO(parking);
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation("Delete By Id")
+    public ResponseEntity delete (@PathVariable String id){
+        parkingService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    @ApiOperation("Create parkings")
+    public ResponseEntity<ParkingDTO> create(@RequestBody ParkingCreateDTO dto) {
+        var parkingCreate = parkingMapper.toParkingCreate(dto);
+        var parking = parkingService.create(parkingCreate);
+        var result = parkingMapper.toParkingDTO(parking);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @PutMapping("/{id}")
+    @ApiOperation("Update parkings")
+    public ResponseEntity<ParkingDTO> update(@PathVariable String id, @RequestBody ParkingCreateDTO parkingCreateDTO) {
+        Parking parkingUpdate = parkingMapper.toParkingCreate(parkingCreateDTO);
+        Parking parking = parkingService.update(id, parkingUpdate);
+        return ResponseEntity.ok(parkingMapper.toParkingDTO(parking));
+    }
+
+    @PostMapping("/{id}")
+    @ApiOperation("Check Out parkings")
+    public ResponseEntity<ParkingDTO> checkOut(@PathVariable String id) {
+        Parking parking = parkingService.checkOut(id);
+        return ResponseEntity.ok(parkingMapper.toParkingDTO(parking));
+    }
+
+}
